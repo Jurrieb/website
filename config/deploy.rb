23ccb833e37d78a007e4e5eeeecf65d59f60f348
Jurgen Bosma <jurrieb@gmail.com>
2013-12-19 14:29:06 +0100

    coole app g

---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+set :rbenv_ruby, "2.0.0-p353"
+set :application, 'website'
+set :repo_url, 'git@github.com:Jurrieb/website.git'
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :scm, :git
+
+set :format, :pretty
+set :log_level, :info
+
+# set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
+set :keep_releases, 5
+
+#set :normalize_asset_timestamps, false
+set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
+
+set :migration_role, 'databaseuser' # Defaults to 'db'
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+
+  end
+
+  # after "deploy", "deploy:migrate"
+
+  after :finishing, 'deploy:cleanup'
+end
+
+namespace :deploy do
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Website::Application.config.secret_key_base = 'c5631fed1c8faf858f899f2785be68b3170c77a7a9ca4d5dee9ef167dfa818999dd8c13929fb5ab237a8e506e49d79b464dfb6e0b7eeb6136de555035d0cefbb'
